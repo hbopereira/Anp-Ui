f3f8204ac040be733bdfbca591bd4f08b4666d4f
unknown <hbopereira>
2018-04-12 09:24:27 -0300

    deploy

--- src/app/ocorrencias/ocorrencia.service.ts
+++ src/app/ocorrencias/ocorrencia.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { AuthHttp } from 'angular2-jwt';
 
+import { environment } from '../../environments/environment';
+
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class OcorrenciaService {
 
-  ocorrenciasUrl = 'http://localhost:8080/ocorrencias';
+  ocorrenciasUrl: string;
 
-  constructor(private http: AuthHttp) { }
+  constructor(private http: AuthHttp) {
+    this.ocorrenciasUrl = `${environment.apiUrl}/ocorrencias`;
+   }
 
   listarTodas(): Promise<any> {
 
